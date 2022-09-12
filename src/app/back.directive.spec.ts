f120412e0fcfb60f3181567f37bbfefffc81cc48
sms165 <61697688+sms165@users.noreply.github.com>
2022-09-12 15:54:37 +0200

    Auto-generated commit [ci skip]

--- src/app/back.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { BackDirective } from './back.directive';
-
-// describe('BackDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new BackDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
